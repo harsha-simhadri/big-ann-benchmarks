77ba9dd7e87851a141c3fde87936d997df1a6369
Martin Aumueller <maau@itu.dk>
2021-04-30 11:33:18 +0200

    Changed structure of data

--- benchmark/datasets.py
+++ benchmark/datasets.py
@@ -2,34 +2,61 @@ import numpy
 import os
 import random
 import sys
-import progressbar
 import struct
+import time
 
 from urllib.request import urlopen
 from urllib.request import urlretrieve
 
-# https://stackoverflow.com/questions/37748105/how-to-use-progressbar-module-with-urlretrieve
-pbar = None
+BASEDIR = "data/"
 
-def show_progress(block_num, block_size, total_size):
-    global pbar
-    if pbar is None:
-        pbar = progressbar.ProgressBar(maxval=total_size)
-        pbar.start()
+# https://blog.shichao.io/2012/10/04/progress_speed_indicator_for_urlretrieve_in_python.html
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+                    (percent, progress_size / (1024 * 1024), speed, duration))
+    sys.stdout.flush()
 
-    downloaded = block_num * block_size
-    if downloaded < total_size:
-        pbar.update(downloaded)
-    else:
-        pbar.finish()
-        pbar = None
+start_time = None
 
 def download(src, dst=None):
-    if not dst:
-        dst = "data/" + src.split("/")[-1]
     if not os.path.exists(dst):
         print('downloading %s -> %s...' % (src, dst))
-        urlretrieve(src, dst, show_progress)
+        urlretrieve(src, dst, reporthook)
+
+def bvecs_mmap(fname):
+    x = numpy.memmap(fname, dtype='uint8', mode='r')
+    d = x[:4].view('int32')[0]
+    return x.reshape(-1, d + 4)[:, 4:]
+
+def ivecs_read(fname):
+    a = numpy.fromfile(fname, dtype='int32')
+    d = a[0]
+    return a.reshape(-1, d + 1)[:, 1:].copy()
+
+# TODO also memmap those?
+def read_fdata(filename, nvecs, dim):
+    with open(filename, 'rb') as f:
+        buf = f.read(dim * 4 * nvecs)
+        vecs = numpy.array(struct.unpack('f' * dim * nvecs, buf))
+    return vecs.reshape((nvecs, dim))
+
+
+def read_idata(filename, nvecs, dim):
+    with open(filename, 'rb') as f:
+        buf = f.read(dim * 4 * nvecs)
+        vecs = numpy.array(struct.unpack('i' * dim * nvecs, buf))
+    return vecs.reshape((nvecs, dim))
+
+def sanitize(x):
+    return numpy.ascontiguousarray(x, dtype='float32')
 
 
 class Dataset():
@@ -76,31 +103,6 @@ class Dataset():
         """
         pass
 
-def bvecs_mmap(fname):
-    x = numpy.memmap(fname, dtype='uint8', mode='r')
-    d = x[:4].view('int32')[0]
-    return x.reshape(-1, d + 4)[:, 4:]
-
-def ivecs_read(fname):
-    a = numpy.fromfile(fname, dtype='int32')
-    d = a[0]
-    return a.reshape(-1, d + 1)[:, 1:].copy()
-
-def read_fdata(filename, nvecs, dim):
-    with open(filename, 'rb') as f:
-        buf = f.read(dim * 4 * nvecs)
-        vecs = numpy.array(struct.unpack('f' * dim * nvecs, buf))
-    return vecs.reshape((nvecs, dim))
-
-
-def read_idata(filename, nvecs, dim):
-    with open(filename, 'rb') as f:
-        buf = f.read(dim * 4 * nvecs)
-        vecs = numpy.array(struct.unpack('i' * dim * nvecs, buf))
-    return vecs.reshape((nvecs, dim))
-
-def sanitize(x):
-    return numpy.ascontiguousarray(x, dtype='float32')
 
 class Sift1B(Dataset):
     def __init__(self, nb_M=1000):
@@ -108,45 +110,51 @@ class Sift1B(Dataset):
         self.nb = 10**6 * nb_M
         self.d = 128
         self.nq = 10000
+        self.ds_fn = "bigann_base.bvecs"
+        self.qs_fn = "bigann_query.bvecs"
+        self.basedir = os.path.join(BASEDIR, "sift1b")
+        if not os.path.exists(self.basedir):
+            os.makedirs(self.basedir)
 
     def prepare(self):
         import gzip, tarfile
-        ds_fn = "bigann_base.bvecs"
-        ds_url = f"ftp://ftp.irisa.fr/local/texmex/corpus/{ds_fn}.gz"
-        qs_fn = "bigann_query.bvecs"
-        qs_url = f"ftp://ftp.irisa.fr/local/texmex/corpus/{qs_fn}.gz"
+        ds_url = f"ftp://ftp.irisa.fr/local/texmex/corpus/{self.ds_fn}.gz"
+        qs_url = f"ftp://ftp.irisa.fr/local/texmex/corpus/{self.qs_fn}.gz"
         gt_fn = "bigann_gnd.tar.gz"
         gt_url = f"ftp://ftp.irisa.fr/local/texmex/corpus/{gt_fn}"
 
-        for fn in [ds_fn, qs_fn]:
-            download(ds_url, f"data/{fn}.gz")
-            with gzip.open(f"data/{fn}.gz") as g:
-                with open(f"data/{fn}", "wb") as f:
-                    f.write(g.read())
+        if not os.path.exists(os.path.join(self.basedir, self.ds_fn)):
+            download(ds_url, os.path.join(self.basedir, f"{self.ds_fn}.gz"))
+            download(qs_url, os.path.join(self.basedir, f"{self.qs_fn}.gz"))
 
-        download(gt_url, f"data/{gt_fn}")
-        with tarfile.open(f"data/{gt_fn}") as f:
-            f.extractall("data/")
+            for fn in [self.ds_fn, self.qs_fn]:
+                with gzip.open(os.path.join(self.basedir, f"{fn}.gz")) as g:
+                    with open(os.path.join(self.basedir,  fn), "wb") as f:
+                        f.write(g.read())
+
+            download(gt_url, os.path.join(self.basedir,gt_fn))
+            with tarfile.open(os.path.join(self.basedir, gt_fn)) as f:
+                f.extractall(self.basedir)
 
     def get_dataset_fn(self):
-        return 'data/bigann_base.bvecs'
+        return os.path.join(self.basedir, self.ds_fn)
 
     def get_dataset(self):
         assert self.nb_M < 100, "dataset too large, use iterator"
-        return sanitize(bvecs_mmap('data/bigann_base.bvecs')[:self.nb])
+        return sanitize(bvecs_mmap(self.get_dataset_fn())[:self.nb])
 
     def get_dataset_iterator(self, bs=512, split=(1,0)):
-        xb = bvecs_mmap('data/bigann_base.bvecs')
+        xb = bvecs_mmap(self.get_dataset_fn())
         nsplit, rank = split
         i0, i1 = self.nb * rank // nsplit, self.nb * (rank + 1) // nsplit
         for j0 in range(i0, i1, bs):
             yield sanitize(xb[j0: min(j0 + bs, i1)])
 
     def get_queries(self):
-        return sanitize(bvecs_mmap('data/bigann_query.bvecs')[:])
+        return sanitize(bvecs_mmap(os.path.join(self.basedir, self.qs_fn))[:])
 
     def get_groundtruth(self, k=None):
-        gt = ivecs_read('data/gnd/idx_%dM.ivecs' % self.nb_M)
+        gt = ivecs_read(os.path.join(self.basedir, 'gnd/idx_%dM.ivecs' % self.nb_M))
         if k is not None:
             assert k <= 100
             gt = gt[:, :k]
