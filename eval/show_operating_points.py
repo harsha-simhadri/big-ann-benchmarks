1d39fe66c4023aee57e6055e6ef982f5c20cba72
Martin Aumueller <maau@itu.dk>
2021-07-21 21:38:47 +0200

    added basic script to extract operating points

---
+++ eval/show_operating_points.py
@@ -0,0 +1,26 @@
+import argparse
+import pandas as pd
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--algorithm',
+        required=True)
+    parser.add_argument(
+        '--threshold',
+        default=10000,
+        help='minimum QPS (10,000 T1/2,000 T2)',
+        type=int)
+    parser.add_argument(
+        'csv',
+        metavar='CSV',
+        help='input csv')
+
+    args = parser.parse_args()
+    df = pd.read_csv(args.csv)
+
+    print(df[(df.qps > args.threshold) & (df.algorithm == args.algorithm)].groupby(['algorithm', 'dataset']).max()[['k-nn', 'ap']])
+
+
+
+
