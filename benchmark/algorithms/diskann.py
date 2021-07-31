b3c1a6ed14f7ca93667742dd20e54f9fd705f2b6
Harsha Vardhan Simhadri <harshasi@microsoft.com>
2021-07-31 01:02:09 -0700

    Added diskannpy instal docker file, empty diskann class

---
+++ benchmark/algorithms/diskann.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import
+import psutil
+import diskannpy
+
+from benchmark.algorithms.base import BaseANN
+from benchmark.datasets import DATASETS, download_accelerated
+
+class DiskANN(BaseANN):
+    def done(self):
+        pass
+
+    def fit(self, dataset):
+        """
+        Build the index for the data points given in dataset name.
+        Assumes that after fitting index is loaded in memory.
+        """
+        pass
+
+    def load_index(self, dataset):
+        """
+        Load the index for dataset. Returns False if index
+        is not available, True otherwise.
+
+        Checking the index usually involves the dataset name
+        and the index build paramters passed during construction.
+        """
+        pass
+
+    def query(self, X, k):
+        """Carry out a batch query for k-NN of query set X."""
+        pass
+
+    def range_query(self, X, radius):
+        """
+        Carry out a batch query for range search with
+        radius.
+        """
+        pass
+
+    def get_results(self):
+        """
+        Helper method to convert query results of k-NN search.
+        If there are nq queries, returns a (nq, k) array of integers
+        representing the indices of the k-NN for each query.
+        """
+        return self.res
+
+    def get_range_results(self):
+        """
+        Helper method to convert query results of range search.
+        If there are nq queries, returns a triple lims, I, D.
+        lims is a (nq) array, such that
+
+            I[lims[q]:lims[q + 1]] in int
+
+        are the indiices of the indices of the range results of query q, and
+
+            D[lims[q]:lims[q + 1]] in float
+
+        are the distances.
+        """
+        return self.res
+
+    def get_additional(self):
+        """
+        Allows to retrieve additional results.
+        """
+        return {}
+
+    def __str__(self):
+        return self.name
