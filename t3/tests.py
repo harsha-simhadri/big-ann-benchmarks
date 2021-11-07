e1cccc0c1264700f42687f02266e3e06d8c00b0d
George Williams <george.williams@gmail.com>
2021-11-07 23:40:52 +0000

    cleaned up, added comments to the unit tests

--- t3/tests.py
+++ t3/tests.py
@@ -15,36 +15,31 @@ if __name__ == "__main__":
 
         # compute recall, don't consider ties
         recall = get_recall_values( (true_ids, true_dists), run_ids, count, False)
-        expected = 1.0  
         print("test: compute recall don't consider ties=",recall[0], "expected=", expected_no_ties)
         if ASSERT:
             assert recall[0]==expected_no_ties
-            print("passed\n")
+            print("test passed\n")
        
         #  compute recall, consider tieds
         recall = get_recall_values( (true_ids, true_dists), run_ids, count, True)
-        expected = 1.0
         print("test: compute recall consider ties=",recall[0], "expected=", expected_with_ties)
         if ASSERT:
             assert recall[0]==expected_with_ties
-            print("passed\n")
+            print("test passed\n")
 
-    # create a query response with no ties, 1 query
+    # create a fake query response with no distance ties, 1 query and k=3
     true_ids    = np.array([ [ 0,   1,   2 ] ])
     true_dists  = np.array([ [ 0.0, 1.0, 2.0 ] ])
     run_ids     = np.array([ [ 0,   1,   2 ] ])
     count=3
-
     test_recall( true_ids, true_dists, run_ids, count, 1.0, 1.0 )
 
-    # create a query response with no ties, 2 queries
+    # create a fake query response with no ties, 2 queries and k=3
     true_ids    = np.array([ [ 0,   1,   2   ], [ 2,   1,   0   ] ])
     true_dists  = np.array([ [ 0.0, 1.0, 2.0 ], [ 0.0, 1.0, 2.0 ] ])
     run_ids     = np.array([ [ 0,   1,   2   ], [ 2,   1,   0   ] ])
     count=3
-
     test_recall( true_ids, true_dists, run_ids, count, 1.0, 1.0 )
-
 
     sys.exit(0)
 
