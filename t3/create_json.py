3ab3b55ad7c2d7eb99696aa62bb86bbe0bb97af0
George Williams <george.williams@gmail.com>
2021-11-13 17:06:01 +0000

    refactored configs

--- t3/create_json.py
+++ t3/create_json.py
@@ -7,53 +7,61 @@ baseline_outpath = "baseline2021.json"
 
 baseline = { 
     "version":          "2021",
-    "deep-1B": {
-        "recall":       [0.942, 2002.490], 
-        "qps":          [3422.473, 0.916],
-        "wspq":         [0.0],      # TODO: placeholder
-        "min-recall":   0.9,
-        "min-qps":      2000,
-        "min-wspq":     0.0         # TODO: placeholder
-    },
-    "bigann-1B": {
-        "recall":       [0.927, 2058.950], 
-        "qps":          [2186.755, 0.905],
-        "wspq":         [0.0],      # TODO: placeholder
-        "min-recall":   0.9,
-        "min-qps":      2000,
-        "min-wspq":     0.0,        # TODO: placeholder
-    },
-    "msturing-1B": {
-        "recall":       [0.910, 2011.542], 
-        "qps":          [2421.856, 0.902],
-        "wspq":         [0.0],      # TODO: placeholder
-        "min-recall":   0.9,
-        "min-qps":      2000,
-        "min-wspq":     0.0         # TODO: placeholder
-    },
-    "msspacev-1B": {
-        "recall":       [0.850, 2190.829], 
-        "qps":          [1484.217, 0.869],
-        "wspq":         [0.0],      # TODO: placeholder
-        "min-recall":   0.9,      # 0.85 # not 0.9 because baseline could not reach 0.9
-        "min-qps":      2000,       # 1484.217,   # not 2K because baseline could not reach 2K
-        "min-wspq":     0.0
-    },
-    "text2image-1B": {
-        "recall":       [0.860, 2120.635],
-        "qps":          [1510.624, 0.882],
-        "wspq":         [0.0],      # TODO: placeholder
-        "min-recall":   0.850,      # 0.86 # not 0.9 because baseline could not reach 0.9
-        "min-qps":      2000,       # 1510.624,   # not 2K because baseline could not reach 2K
-        "min-wspq":     0.0         # TODO: placeholder
-    },
-    "ssnpp-1B": {
-        "recall":       [0.978634, 2907.413803], # from focused2.csv
-        "qps":          [5572.272416, 0.910180], # from focused2.csv
-        "wspq":         [0.0],      # TODO: placeholder
-        "min-recall":   0.9,        # TODO: placeholder
-        "min-qps":      2000,       # TODO: placeholder
-        "min-wspq":     0.0         # TODO: placeholder
+    "datasets": {
+        "deep-1B": {
+            "recall":       [0.942, 2002.490], 
+            "qps":          [3422.473, 0.916],
+            "wspq":         [0.0],      
+            "cost":         0.0,
+            "min-recall":   0.9,
+            "min-qps":      2000,
+            "min-wspq":     0.0         # No min wspq
+        },
+        "bigann-1B": {
+            "recall":       [0.927, 2058.950], 
+            "qps":          [2186.755, 0.905],
+            "wspq":         [0.0],      # TODO: placeholder
+            "cost":         0.0,
+            "min-recall":   0.9,
+            "min-qps":      2000,
+            "min-wspq":     0.0,        # No min wspq
+        },
+        "msturing-1B": {
+            "recall":       [0.910, 2011.542], 
+            "qps":          [2421.856, 0.902],
+            "wspq":         [0.0],      # TODO: placeholder
+            "cost":         0.0,
+            "min-recall":   0.9,
+            "min-qps":      2000,
+            "min-wspq":     0.0         # No min wspq
+        },
+        "msspacev-1B": {
+            "recall":       [0.850, 2190.829], 
+            "qps":          [1484.217, 0.869],
+            "wspq":         [0.0],      # TODO: placeholder
+            "cost":         0.0,
+            "min-recall":   0.9,        
+            "min-qps":      2000,       
+            "min-wspq":     0.0         # No min wspq
+        },
+        "text2image-1B": {
+            "recall":       [0.860, 2120.635],
+            "qps":          [1510.624, 0.882],
+            "wspq":         [0.0],      
+            "cost":         0.0,
+            "min-recall":   0.850,      # 0.9 because baseline could not reach 0.9
+            "min-qps":      2000,       
+            "min-wspq":     0.0         # No min wspq
+        },
+        "ssnpp-1B": {
+            "recall":       [0.978634, 2907.413803], 
+            "qps":          [5572.272416, 0.910180], 
+            "wspq":         [0.0],      # TODO: placeholder
+            "cost":         0.0,
+            "min-recall":   0.9,       
+            "min-qps":      2000,       
+            "min-wspq":     0.0         # No min wspq
+        }
     }
 }
 
@@ -65,10 +73,13 @@ print("Wrote baseline json at %s" % baseline_outpath )
 competition_outpath = "competition2021.json"
 
 competition = {
-    "version":  "2021", 
-    "kwh_cost": 0.10, # 10 cents
-    "cost_qps": 100000, # 100,000 queries per second
-    "opex_time": 4 # number of years to evaluate opex
+    "version":          "2021", 
+    "kwh_cost":         0.10,   # 10 cents
+    "cost_qps":         100000, # 100,000 queries per second
+    "opex_time":        4,      # number of years to evaluate opex
+    "min_qual_dsets":   3,
+    "max_run_params":   10
+    
 }
 
 with open(competition_outpath, 'w') as outfile:
