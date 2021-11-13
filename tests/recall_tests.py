b896017fd17cd2d9c597950fd40363edaf233b1e
George Williams <george.williams@gmail.com>
2021-11-13 14:52:12 -0800

    added recall tests with multiple independent tie groups

--- tests/recall_tests.py
+++ tests/recall_tests.py
@@ -112,6 +112,20 @@ def main_tests():
     run_ids     = np.array([ [ 0,   1,                  5   ], [ 5,   4,             1        ] ])
     count=3
     test_recall( true_ids, true_dists, run_ids, count, np.mean([2.0, 2.0])/float(count), np.mean([2.0, 3.0])/float(count) )
+    
+    print("TEST: fake query response with two independent tie groups, 2 queries and k=3")
+    true_ids    = np.array([ [ 0,   1,   2,   3,   4,   5   ], [ 5,   4,   3,   2,   1,   0   ] ])
+    true_dists  = np.array([ [ 0.0, 0.0, 2.0, 3.0, 3.0, 4.0 ], [ 0.0, 0.0, 2.0, 3.0, 3.0, 4.0 ] ])
+    run_ids     = np.array([ [ 0,   1,                  5   ], [ 5,   4,             1        ] ])
+    count=3
+    test_recall( true_ids, true_dists, run_ids, count, np.mean([2.0, 2.0])/float(count), np.mean([2.0, 3.0])/float(count) )
+    
+    print("TEST: fake query response with two independent tie groups, 2 queries and k=4")
+    true_ids    = np.array([ [ 0,   1,   2,   3,   4,   5,   6   ], [ 6,   5,   4,   3,   2,   1,   0   ] ])
+    true_dists  = np.array([ [ 0.0, 0.0, 2.0, 3.0, 3.0, 4.0, 5.0 ], [ 0.0, 0.0, 2.0, 3.0, 3.0, 4.0, 5.0 ] ])
+    run_ids     = np.array([ [ 0,   1,                  5,   7   ], [ 5,   4,             1,        7   ] ])
+    count=4
+    test_recall( true_ids, true_dists, run_ids, count, np.mean([2.0, 2.0])/float(count), np.mean([3.0, 3.0])/float(count) )
 
     #
     # dataset tests
