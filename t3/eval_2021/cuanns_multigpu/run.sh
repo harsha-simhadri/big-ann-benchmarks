7fed1f5b08c4a996d111c11b13b729f276ef0dfa
George Williams <george.williams@gmail.com>
2021-11-22 22:26:40 -0800

    added eval run script

---
+++ t3/eval_2021/cuanns_multigpu/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+python run.py --definitions t3/cuanns_multigpu/algos.yaml --dataset deep-1B --t3  --power-capture 10.150.170.104:1235:10
+python run.py --definitions t3/cuanns_multigpu/algos.yaml --dataset msturing-1B --t3  --power-capture 10.150.170.104:1235:10
+python run.py --definitions t3/cuanns_multigpu/algos.yaml --dataset msspacev-1B --t3 --power-capture 10.150.170.104:1235:10
+python run.py --definitions t3/cuanns_multigpu/algos.yaml --dataset bigann-1B --t3 --power-capture 10.150.140.104:1235:10
+
