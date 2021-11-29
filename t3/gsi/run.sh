93757044778d15294bcf420c46d905606abbb62e
George Williams <george.williams@gmail.com>
2021-11-29 21:44:03 +0000

    gsi-based run script

--- t3/gsi/run.sh
+++ t3/gsi/run.sh
@@ -2,7 +2,7 @@
 
 set -x 
 
-LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset ssnpp-1B --power-capture 192.168.99.32:1236:10
+LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset text2image-1B --power-capture 192.168.99.32:1236:10
 
 #gdb -ex=r --args env LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset ssnpp-1B | tee "$LOGFILE"
 #| tee "$LOGFILE"
