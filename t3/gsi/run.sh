e7fd28dbe50b8acf965b324b9addc8c0abf6d9c3
George Williams <george.williams@gmail.com>
2021-12-02 09:43:16 +0000

    now ssnpp

--- t3/gsi/run.sh
+++ t3/gsi/run.sh
@@ -6,7 +6,7 @@ set -x
 #LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset deep-1B --power-capture 192.168.99.32:1236:10
 #LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset bigann-1B --power-capture 192.168.99.32:1236:10
 #LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset msturing-1B --power-capture 192.168.99.32:1236:10
-LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset msspacev-1B --power-capture 192.168.99.32:1236:10
-#LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset ssnpp-1B --power-capture 192.168.99.32:1236:10
+#LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset msspacev-1B --power-capture 192.168.99.32:1236:10
+LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --nodocker --definitions t3/gsi/algos.yaml --dataset ssnpp-1B --power-capture 192.168.99.32:1236:10
 
 
