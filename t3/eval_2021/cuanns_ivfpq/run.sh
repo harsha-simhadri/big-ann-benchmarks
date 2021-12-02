43cf5274d67b0721273d1c1f74db5ed1f88b9573
George Williams <george.williams@gmail.com>
2021-12-01 21:31:20 -0800

    only running t2i

--- t3/eval_2021/cuanns_ivfpq/run.sh
+++ t3/eval_2021/cuanns_ivfpq/run.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset deep-1B --t3  --power-capture 10.150.170.104:1235:10
-python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset msturing-1B --t3  --power-capture 10.150.170.104:1235:10
-python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset msspacev-1B --t3 --power-capture 10.150.170.104:1235:10
-python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset bigann-1B --t3 --power-capture 10.150.170.104:1235:10
+#python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset deep-1B --t3  --power-capture 10.150.170.104:1235:10
+#python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset msturing-1B --t3  --power-capture 10.150.170.104:1235:10
+#python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset msspacev-1B --t3 --power-capture 10.150.170.104:1235:10
+#python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset bigann-1B --t3 --power-capture 10.150.170.104:1235:10
 python run.py --definitions t3/cuanns_ivfpq/algos.yaml --dataset text2image-1B --t3 --power-capture 10.150.170.104:1235:10
 
