558ec5f0b1e2bb68feb4a18d7fabb4b664fa235c
George Williams <george.williams@gmail.com>
2021-12-03 19:20:04 +0000

    adding private run script

---
+++ t3/eval_2021/gemini/prun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x 
+
+#LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --private-query --nodocker --definitions t3/gsi/algos.yaml --dataset text2image-1B --power-capture 192.168.99.32:1236:10
+LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --private-query --nodocker --definitions t3/gsi/algos.yaml --dataset deep-1B --power-capture 192.168.99.32:1236:10
+#LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --private-query --nodocker --definitions t3/gsi/algos.yaml --dataset bigann-1B --power-capture 192.168.99.32:1236:10
+#LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --private-query --nodocker --definitions t3/gsi/algos.yaml --dataset msturing-1B --power-capture 192.168.99.32:1236:10
+#LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --private-query --nodocker --definitions t3/gsi/algos.yaml --dataset msspacev-1B --power-capture 192.168.99.32:1236:10
+#LD_LIBRARY_PATH=./gsl_resources_release PYTHONPATH=./gsl_resources_release python3 run.py --t3 --private-query --nodocker --definitions t3/gsi/algos.yaml --dataset ssnpp-1B --power-capture 192.168.99.32:1236:10
+
+
