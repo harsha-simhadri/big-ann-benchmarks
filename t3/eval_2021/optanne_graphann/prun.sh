6fef9caa3bf421d92023a2c6a6678716967a8c88
George Williams <george.williams@gmail.com>
2021-12-05 17:28:49 -0500

    re-eval on synced config just t2i and msturing

--- t3/eval_2021/optanne_graphann/prun.sh
+++ t3/eval_2021/optanne_graphann/prun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset bigann-1B --power-capture 192.168.0.121:14118:10
-python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset msspacev-1B --power-capture 192.168.0.121:14118:10
-python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset deep-1B --power-capture 192.168.0.121:14118:10
+#python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset bigann-1B --power-capture 192.168.0.121:14118:10
+#python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset msspacev-1B --power-capture 192.168.0.121:14118:10
+#python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset deep-1B --power-capture 192.168.0.121:14118:10
 python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset msturing-1B --power-capture 192.168.0.121:14118:10
 python run.py --algorithm graphann --definitions t3/optanne_graphann/algos.yaml --t3 --private-query --nodocker --dataset text2image-1B --power-capture 192.168.0.121:14118:10
 
