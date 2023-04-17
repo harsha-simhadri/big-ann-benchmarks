8981311601c3a0b64252b676fc436179cbd344f4
Matthijs Douze <matthijs@fb.com>
2023-04-17 05:31:26 -0700

    write all

--- dataset_preparation/prepare_yfcc100m.py
+++ dataset_preparation/prepare_yfcc100m.py
@@ -1,6 +1,7 @@
 
 import os
 import numpy as np
+import pickle
 from scipy.sparse import csr_matrix
 
 try:
@@ -13,7 +14,8 @@ except ImportError:
 ds = datasets.YFCC100MDataset()
 print(ds)
 
-todo = "query_vecs"
+# todo = "query_vecs query_meta database_vecs database_meta"
+todo = "query_vecs query_meta"
 
 
 # for now, we work with random descriptors
@@ -31,60 +33,115 @@ all_descriptors = np.memmap(
 )
 
 # simulate feature extraction failure: we have a flag for each descriptor about validity
-
 all_descriptors_valid = np.fromfile(
     tmpdir + "random_yfcc100m_descriptors.valid.uint8",
     count=10**8, dtype='uint8'
 )
 # make boolean array
-all_descriptors_valid = all_descriptors_valid < 200
+all_descriptors_valid = all_descriptors_valid < 230
+print(f"valid descriptors: {all_descriptors_valid.sum()}/{all_descriptors_valid.size}")
 
 
+fname = metadata_dir + "query_array.npy"
+print("load query metadata", fname)
+query_array = np.load(fname)
+# format: 200k rows 4 columns with of word1, word2, frequency, query vector id
+# word2 may be -1 for single-word queries
+print("   size", query_array.shape)
 
-if "query_vecs" in todo:
-    voc_size = 200000 + 386
-    fname = metadata_dir + "query_array.npy"
-    print("load query metadata", fname)
-    query_array = np.load(fname)
-    # format: 200k rows 4 columns with of word1, word2, frequency, query vector id
-    # word2 may be -1 for single-word queries
-    print("   size", query_array.shape)
+qids = query_array[:, 3]
+# filter out invalid ones
+valid_mask = all_descriptors_valid[qids]
+print(f"valid mask size: {valid_mask.sum()}/{valid_mask.size}")
+voc_size = 200000 + 386
 
-    qids = query_array[:, 3]
-    # filter out invalid ones
-    valid_mask = all_descriptors_valid[qids]
+
+if "query_vecs" in todo or "query_meta" in todo:
 
     # public queries: the 100k first ones
-    subset = np.where(valid_mask)[0][:100000]
+    subset = np.where(valid_mask)[0][:200000]
 
-    # pick the descriptors
-    descs = all_descriptors[qids[subset]]
+    if "query_vecs" in todo:
+        # pick the descriptors
+        descs = all_descriptors[qids[subset]]
 
-    # write out
-    fname = os.path.join(ds.basedir, ds.qs_fn)
-    print("write", fname)
-    dataset_io.u8bin_write(descs, fname)
+        # write out
+        fname = os.path.join(ds.basedir, ds.qs_fn)
+        fname2 = os.path.join(ds.basedir, ds.qs_private_fn)
+        print("write", fname, fname2)
+        dataset_io.u8bin_write(descs[:100000], fname)
+        dataset_io.u8bin_write(descs[100000:200000], fname2)
 
-    # build query term sparse matrix
-    is_2word = query_array[:, 1] >= 0
-    words_per_q = 1 + is_2word.astype(int)
-    nq = len(query_array)
-    indptr = np.zeros(nq + 1, dtype=int)
-    indptr[1:] = np.cumsum(words_per_q)
-    indices = query_array[:, :2].ravel()
-    indices = indices[indices >= 0]
-    nnz = indices.size
-    queries_sparse = csr_matrix(
-        (np.ones(nnz, dtype='float32'), indices, indptr),
-        shape = (nq, voc_size)
-    )
+    if "query_meta" in todo:
+        # build query term sparse matrix
+        is_2word = query_array[:, 1] >= 0
+        words_per_q = 1 + is_2word.astype(int)
+        nq = len(query_array)
+        indptr = np.zeros(nq + 1, dtype=int)
+        indptr[1:] = np.cumsum(words_per_q)
+        indices = query_array[:, :2].ravel()
+        indices = indices[indices >= 0]
+        nnz = indices.size
+        queries_sparse = csr_matrix(
+            (np.ones(nnz, dtype='float32'), indices, indptr),
+            shape = (nq, voc_size)
+        )
+        print("  queries size: ", queries_sparse.shape)
+        queries_sparse = queries_sparse[valid_mask]
+        print("  valid queries size: ", queries_sparse.shape)
 
-    # write query metadata
-    fname = os.path.join(ds.basedir, ds.qs_metadata_fn)
-    print("write", fname)
-    dataset_io.write_sparse_matrix(queries_sparse[:100000], fname)
+        # write query metadata
+        fname = os.path.join(ds.basedir, ds.qs_metadata_fn)
+        fname2 = os.path.join(ds.basedir, ds.qs_private_metadata_fn)
+        print("write", fname, fname2)
+        dataset_io.write_sparse_matrix(queries_sparse[:100000], fname)
+        dataset_io.write_sparse_matrix(queries_sparse[100000:200000], fname2)
 
+if "database_vecs" in todo or "database_meta" in todo:
+    available_mask = all_descriptors_valid.copy()
+    available_mask[qids] = False
 
+    print(f"Available for use in the database: "
+          f"{available_mask.sum()}/{available_mask.size}")
 
+    nb = 10_000_000
+
+    rs = np.random.RandomState(123)
+    available = np.where(available_mask)[0]
+    subset = rs.choice(available, nb)
+    subset.sort()  # may be useful to not randomly access the vectors
+
+    print(f"selected {len(subset)} / {len(available)} vectors")
+
+    if "database_vecs" in todo:
+        descs = all_descriptors[subset]
+
+        fname = os.path.join(ds.basedir, ds.ds_fn)
+        print("write", fname)
+        dataset_io.u8bin_write(descs, fname)
+
+    if "database_meta" in todo:
+
+        # handle metadata
+        fname = metadata_dir + "combined_matrix.pickle"
+        print("loading", fname)
+        combined_data = pickle.load(open(fname, "rb"))
+
+        combined_words = combined_data["combined_words"]
+        combined_matrix = combined_data["combined_matrix"]
+
+        print(f"loaded metadta shape {combined_matrix.shape} "
+              f"nnz={combined_matrix.nnz}")
+        fname = os.path.join(ds.basedir, "words.txt")
+        print("write", fname)
+        open(fname, "w").write("\n".join(combined_words))
+
+        database_matrix = combined_matrix[subset]
+        print(f"subset shape {database_matrix.shape} "
+              f"nnz={database_matrix.nnz}")
+
+        fname = os.path.join(ds.basedir, ds.ds_metadata_fn)
+        print("write", fname)
+        dataset_io.write_sparse_matrix(database_matrix, fname)
 
 
