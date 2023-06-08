083fe91e500f3db8f3df2b5e7addc6e44e1eca3f
Matthijs Douze <matthijs@fb.com>
2023-06-08 09:33:38 -0700

    cleanup

--- run.py
+++
@@ -1,6 +0,0 @@
-from benchmark.main import main
-from multiprocessing import freeze_support
-
-if __name__ == "__main__":
-    freeze_support()
-    main()
--- t3/gemini/buildidx/run_bin_build_index.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-PATH=/usr/bin:$PATH python3 build_index.py
