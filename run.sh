03874a76f26add9ca57aa7d632350a8782e04068
Carlos Lassance <cadurosar@gmail.com>
2023-10-16 07:03:12 +0000

    remove run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-python install.py --neurips23track sparse    --algorithm nle
-for algorithm in NLE-10 NLE
-do
-    for dataset in sparse-small sparse-1M sparse-full
-    do
-        python run.py --neurips23track sparse    --algorithm $algorithm --dataset $dataset
-    done
-done
