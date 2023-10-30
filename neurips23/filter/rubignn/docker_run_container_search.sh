a24ce0cc56a238a1d140580b1aaa3164cc1cd3d0
Ubuntu <ubuntu@ip-172-31-86-248.ec2.internal>
2023-10-30 07:55:21 +0000

    update

--- neurips23/filter/rubignn/docker_run_container_search.sh
+++ neurips23/filter/rubignn/docker_run_container_search.sh
@@ -1,8 +1,8 @@
 CONTEST_REPO_PATH=/home/ubuntu/big-ann-benchmarks #path to big-ann-benchmarks directory
-INDEX_FILE_PATH=/home/ubuntu/index_file #path to index_file directory
+INDEX_FILE_PATH=/home/ubuntu/built_index #path to index_file directory
 
-docker container run -it  --mount type=bind,src=$CONTEST_REPO_PATH/results,dst=/home/app/results --mount type=bind,src=$INDEX_FILE_PATH,dst=/home/app/index_file --read-only --mount type=bind,src=$CONTEST_REPO_PATH/data,dst=/home/app/data  neurips23-filter-rubignn  /bin/bash -c 'mkdir -p /home/app/results/neurips23/filter/yfcc-10M/10/rubignn && 
+docker container run -it  --mount type=bind,src=$CONTEST_REPO_PATH/results,dst=/home/app/results --mount type=bind,src=$INDEX_FILE_PATH/index_file_96,dst=/home/app/index_file --read-only --mount type=bind,src=$CONTEST_REPO_PATH/data,dst=/home/app/data  neurips23-filter-rubignn  /bin/bash -c 'mkdir -p /home/app/results/neurips23/filter/yfcc-10M/10/rubignn && 
 cd /home/app/ru-bignn-23/build &&
-./apps/search_contest --index_path_prefix /home/app/index_file/yfcc_R16_L80_SR80_stitched_index_label --query_file /home/app/data/yfcc100M/query.public.100K.u8bin --search_list 50 80 90 100 110 120 130 --query_filters_file /home/app/data/yfcc100M/query.metadata.public.100K.spmat --result_path_prefix /home/app/results/neurips23/filter/yfcc-10M/10/rubignn/rubignn'
+./apps/search_contest --index_path_prefix /home/app/index_file/yfcc_R16_L80_SR96_stitched_index_label --query_file /home/app/data/yfcc100M/query.public.100K.u8bin --search_list 50 80 90 100 110 120 130 --query_filters_file /home/app/data/yfcc100M/query.metadata.public.100K.spmat --result_path_prefix /home/app/results/neurips23/filter/yfcc-10M/10/rubignn/rubignn'
 
 docker container prune
--- neurips23/filter/rubignn/download_index.sh
+++ neurips23/filter/rubignn/download_index.sh
@@ -1,1 +1,2 @@
-azcopy copy
+INDEX_FILE_PATH=/home/ubuntu/built_index
+azcopy copy 'https://rubignn.blob.core.windows.net/biganncontest-96/index_file_96?sp=rl&st=2023-10-30T07:40:31Z&se=2023-12-01T16:40:31Z&spr=https&sv=2022-11-02&sr=c&sig=Xj6pXXVsTMAJ2K7sHX0H3qyqr5E%2BnN%2FOyvczZ3%2Bz2fo%3D' $INDEX_FILE_PATH --recursive
