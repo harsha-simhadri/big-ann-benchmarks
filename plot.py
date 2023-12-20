8b4353e6dbe7a2e1a79dadfe7a2536d12926c265
Amir Ingber <93526572+ingberam@users.noreply.github.com>
2023-12-20 13:44:37 +0200

    add provate-query flag to plot.py (#260)

--- plot.py
+++ plot.py
@@ -139,6 +139,10 @@ if __name__ == "__main__":
         choices=['filter', 'ood', 'sparse', 'streaming', 'none'],
         default='none'
     )
+    parser.add_argument(
+        '--private-query',
+        help='Use the private queries and ground truth',
+        action='store_true')
     args = parser.parse_args()
 
     if not args.output:
@@ -155,8 +159,13 @@ if __name__ == "__main__":
     unique_algorithms = get_unique_algorithms()
     results = load_all_results(args.dataset, count, neurips23track=args.neurips23track)
     linestyles = create_linestyles(sorted(unique_algorithms))
-    runs = compute_metrics(dataset.get_groundtruth(k=args.count),
-                           results, args.x_axis, args.y_axis, args.recompute)
+    if args.private_query:
+        runs = compute_metrics(dataset.get_private_groundtruth(k=args.count),
+                               results, args.x_axis, args.y_axis, args.recompute)
+    else:
+        runs = compute_metrics(dataset.get_groundtruth(k=args.count),
+                               results, args.x_axis, args.y_axis, args.recompute)
+
     if not runs:
         raise Exception('Nothing to plot')
 
