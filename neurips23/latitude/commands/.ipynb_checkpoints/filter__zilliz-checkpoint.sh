3d0d3c9314184a3542076323d316378646abe8b9
sourcesync <george.williams@gmail.com>
2024-08-30 19:45:18 +0000

    cleanup jupyter checkpoints dir

--- neurips23/latitude/commands/.ipynb_checkpoints/filter__zilliz-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x # echo the command
-set -e # stop script on error
-
-python install.py --neurips23track filter --algorithm zilliz
-python3 run.py --dataset yfcc-10M --algorithm zilliz --neurips23track filter 
